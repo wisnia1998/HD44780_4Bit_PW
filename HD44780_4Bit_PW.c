377931acf3d23e101c1e00ce01b96ba84b801cda
wisnia1998 <48693427+wisnia1998@users.noreply.github.com>
2019-10-29 16:59:46 +0100

    First almost working version od library

---
+++ HD44780_4Bit_PW.c
@@ -0,0 +1,74 @@
+#include "HD44780_4Bit_PW.h"
+
+
+
+void LCD_Init()
+{
+	DDR_E  |= PIN_E;
+	DDR_RS |= PIN_RS;
+	DDR_D4 |= PIN_D4;
+	DDR_D5 |= PIN_D5;
+	DDR_D6 |= PIN_D6;
+	DDR_D7 |= PIN_D7;
+	_delay_ms(15);
+	LCD_WriteCommand(LCD_COMMAND_FUNCTION_SET);
+	_delay_ms(5);
+	LCD_WriteCommand(LCD_COMMAND_FUNCTION_SET);
+	_delay_ms(0.1);
+	LCD_WriteCommand(LCD_COMMAND_FUNCTION_SET);
+	_delay_ms(1);
+	LCD_WriteCommand(LCD_COMMAND_FUNCTION_SET | LCD_PARAM_FUNCTION_SET_2LINES);
+}
+
+void LCD_Write(uint8_t data, uint8_t RS)
+{
+	if (RS){	RS_High;	}
+	else {	 RS_Low;		}
+	
+	Enable_High;
+	_delay_us(1);
+	
+	PORT_D4 &= ~PIN_D4;
+	PORT_D5 &= ~PIN_D5;
+	PORT_D6 &= ~PIN_D6;
+	PORT_D7 &= ~PIN_D7;
+	
+	#ifdef Diferent_data_pin
+	if (data & 0x01)
+	{
+		PORT_D4 |= PIN_D4;
+	}
+	if (data & 0x02)
+	{
+		PORT_D5 |= PIN_D5;
+	}
+	if (data & 0x04)
+	{
+		PORT_D6 |= PIN_D6;
+	}
+	if (data & 0x08)
+	{
+		PORT_D7 |= PIN_D7;
+	}
+	#else
+	Data_Port |= data & Data_4bit_Mask;
+	#endif
+	_delay_us(1);
+	Enable_Low;
+	
+}
+
+void LCD_WriteData(uint8_t Data)
+{
+	LCD_Write(Data, 1);
+	_delay_ms(1);
+	LCD_Write(Data >> 4, 1);
+}
+
+void LCD_WriteCommand(uint8_t Command)
+{
+	LCD_Write(Command, 0);
+	_delay_ms(1);
+	LCD_Write(Command >> 4, 0);
+}
+
---
+++ HD44780_4Bit_PW.h
@@ -0,0 +1,85 @@
+/*
+ *HD44780_4Bit_PW.h
+ *Title: HD44780 library for 4-Bit control
+ *Author: Pawe³ Waœniowski
+ *Created: 23.10.2019
+*/
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+#define Data_4bit_Mask 0x0f
+//////////////////Pin control declaration/////////////////////////////////
+#define Diferent_data_pin
+#define PIN_RS	(1 << PORTB1)
+#define PORT_RS	PORTB
+#define DDR_RS	DDRB
+#define PIN_E	(1 << PORTB0)
+#define PORT_E	PORTB
+#define DDR_E	DDRB
+//#define PIN_RW	(1 << PORTB3)
+//#define PORT_RW	PORTB
+//#define DDR_RW	DDRB
+//Control pin
+#ifdef Diferent_data_pin
+#define PIN_D4	(1 << PORTD0)
+#define PORT_D4	PORTD
+#define DDR_D4	DDRD
+#define PIN_D5	(1 << PORTD1)
+#define PORT_D5	PORTD
+#define DDR_D5	DDRD
+#define PIN_D6	(1 << PORTD2)
+#define PORT_D6	PORTD
+#define DDR_D6	DDRD
+#define PIN_D7	(1 << PORTD3)
+#define PORT_D7	PORTD
+#define DDR_D7	DDRD
+#else
+#define Data_Port PORTD
+#define Data_Direct DDRD
+#define PIN_D4	(1 << PORTD0)
+#define PIN_D5	(1 << PORTD1)
+#define PIN_D6	(1 << PORTD2)
+#define PIN_D7	(1 << PORTD3)
+#endif
+//////////////////////////////////////////////////////////////////////////
+
+///////////////////////////MAKRO//////////////////////////////////////////
+#define Enable_High		PORT_E |= PIN_E
+#define Enable_Low		PORT_E &= ~PIN_E
+#define RS_High		PORT_RS |= PIN_RS
+#define RS_Low		PORT_RS &= ~PIN_RS
+//////////////////////////////////////////////////////////////////////////
+
+/////////////////////////LCD COMMAND//////////////////////////////////////
+#define LCD_COMMAND_CLEAR_SCREEN 0x01
+#define LCD_COMMAND_RETURN_HOME 0x02
+#define LCD_COMMAND_ENTRY_MODE_SET 0x04
+#define LCD_COMMAND_ON_OFF 0x08
+#define LCD_COMMAND_SHIFT 0x10
+#define LCD_COMMAND_FUNCTION_SET 0x20
+#define LCD_COMMAND_SET_CGRAM_ADDRESS 0x40
+#define LCD_COMMAND_SET_DDRAM_ADDRESS 0x80
+/********************************************************/
+#define LCD_PARAM_ENTRY_MODE_SET_SHIFT 0x01
+#define LCD_PARAM_ENTRY_MODE_SET_INCREMENT 0x02
+#define LCD_PARAM_ON_OFF_BLINK 0x01
+#define LCD_PARAM_ON_OFF_CURSOR 0x02
+#define LCD_PARAM_ON_OFF_DISPLAY 0x04
+#define LCD_PARAM_SHIFT_RIGHT 0x04
+#define LCD_PARAM_SHIFT_DISPLAY 0x08
+#define LCD_PARAM_FUNCTION_SET_5X10 0x04
+#define LCD_PARAM_FUNCTION_SET_2LINES 0x08
+#define LCD_PARAM_FUNCTION_SET_8BIT 0x10
+//////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////Function///////////////////////////////////
+void LCD_Init();
+void LCD_Write(uint8_t data, uint8_t RS);
+void LCD_WriteData(uint8_t Data);
+void LCD_WriteCommand(uint8_t Command);
+//////////////////////////////////////////////////////////////////////////
